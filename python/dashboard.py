14a5bc8b6d1745e83af673dd7dc407e1b28436d8
GreatGandalf <levi.staal15@gmail.com>
2018-10-22 11:32:20 +0200

    labels and stuff

--- python/dashboard.py
+++ python/dashboard.py
@@ -4,7 +4,24 @@ import sched, time
 
 top = tkinter.Tk()
 
-can = tkinter.Canvas(top, height=700, width=1200)
+can = tkinter.Canvas(top, height=700, width=200)
+can.pack(side="left")
+can1 = tkinter.Canvas(top, height=700, width=800)
+can1.pack(side="left")
 
-can.pack()
+tempLabel = tkinter.Label(can,text="Temperatuursgrenzen:")
+tempLabel.pack()
+
+minTempLabel = tkinter.Label(can,text="Minimumgrens:")
+minTempLabel.pack()
+minTemp = tkinter.Entry(can)
+minTemp.pack()
+
+maxTempLabel = tkinter.Label(can,text="Maximumgrens:")
+maxTempLabel.pack()
+maxTemp = tkinter.Entry(can)
+maxTemp.pack()
+
+line = canvas.create_line(x0, y0, x1, y1, ..., xn, yn, options)
+
 top.mainloop()
