6b402658b3ec79beaae8285c7263b189ac549a20
kevinhoukema1 <kevin-houkema@hotmail.com>
2018-11-06 18:55:35 +0100

    Upload main.c
    Dit is de versie waarbij de Licht en Hitte sensor werken. Er zit een stukje in voor de afstands sensor in maar dat werkt nog niet.

--- c/main.c
+++ c/main.c
@@ -1,6 +1,35 @@
 #include "AVR_TTC_scheduler.h"
+
 #include <avr/io.h>
+#include <stdint.h>
+#define F_CPU 16E6
+#include <util/delay.h>
 #include <avr/interrupt.h>
+#include <avr/sfr_defs.h>
+	
+#define UBBRVAL 51
+
+#define LTH 200 //light threshold high
+#define LTL 160 //light threshold Low
+#define HTH 24	//Heat threshold high
+#define HTL 20	//Heat threshold low
+
+#define HIGH 0x01
+#define LOW  0x0
+
+// const uint8_t data = 0;
+// const uint8_t clock = 1;
+// const uint8_t strobe = 2;
+// uint16_t countNumber = 0;
+// uint8_t clear= 0;
+
+uint8_t status = 0;
+
+int Lstand = 0;
+int LstandCount = 0;
+
+int Htemp = 0;
+int HtempCount = 0;
 
 // The array of tasks
 sTask SCH_tasks_G[SCH_MAX_TASKS];
@@ -222,10 +251,246 @@ ISR(TIMER1_COMPA_vect)
    }
 }
 
-// ------------------------------------------------------------------
+// ---------------------//
+//End copy pasta Scheduler.c
+// ---------------------//
+// Eigen code			//
+//----------------------//
 
+/*
+	ADCsingleREAD leest waardes van een poort
+
+*/
+int ADCsingleREAD(uint8_t adctouse)
+{
+	int ADCval;
+
+	ADMUX = adctouse;        // use #1 ADC
+	ADMUX |= (1 << REFS0);   // use AVcc as the reference
+	ADMUX &= ~(1 << ADLAR);  // clear for 10 bit resolution
+
+	// 128 prescale for 8Mhz
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+	ADCSRA |= (1 << ADEN);    // Enable the ADC
+	ADCSRA |= (1 << ADSC);    // Start the ADC conversion
+
+	while(ADCSRA & (1 << ADSC)); // waits for the ADC to finish
+
+	ADCval = ADCL;
+	ADCval = (ADCH << 8) + ADCval; // ADCH is read so ADC can be updated again
+
+	return ADCval;
+}
+
+
+
+void uart_init()
+{
+	// set the baud rate
+	UBRR0H = 0;
+	UBRR0L = UBBRVAL;
+	// disable U2X mode
+	UCSR0A = 0;
+	// enable transmitter
+	UCSR0B = _BV(TXEN0) | _BV(RXEN0);
+	// set frame format : asynchronous, 8 data bits, 1 stop bit, no parity
+	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+}
+
+void transmit(uint8_t data)
+{
+	// wait for an empty transmit buffer
+	// UDRE is set when the transmit buffer is empty
+	loop_until_bit_is_set(UCSR0A, UDRE0);
+	// send the data
+	UDR0 = data;
+}
+uint8_t receive(void){
+	loop_until_bit_is_set(UCSR0A, RXC0);
+	return UDR0;
+}
+
+
+void on(char pin){
+	DDRB |= (1 << pin);
+	PINB |= (1 << pin);
+}
+
+/*
+	Simuleert de LED lampjes om de rolluik uit te rollen
+*/
+void simulateOUT(){
+	on(PB3);
+	_delay_ms(5000);
+	on(PB2);
+	_delay_ms(1000);
+}
+
+void off(char pin){
+	DDRB |= (0 << pin);
+	PINB |= (0 << pin);
+}
+
+/*
+	Simuleert de LED lampjes om de rolluik op te rollen
+*/
+void simulateIN(){
+	on(PB3);
+	_delay_ms(5000);
+	on(PB4);
+	_delay_ms(1000);
+}
+
+
+//------------------------------------------//
+//				 SensorLogic				//
+
+//----------HeatSensor----------//
+int hitteSensor(){
+	double number1 = 0;
+	int number2 = 0;
+	number1 = ADCsingleREAD(1);
+	number1 = number1 / 1024;   //find percentage of input reading
+	number1 = number1 * 5;                     //multiply by 5V to get voltage
+	number1 = number1 - 0.5;                   //Subtract the offset
+	number1 = number1 * 100;                   //Convert to degrees
+	//number2 = (int) number1; -> voor display 
+	//transmit(number1);
+	number1 = (int)number1;
+	return number1;
+}
+
+// add current value to total heat value and count++
+void countHS(){
+	Htemp += hitteSensor();
+	HtempCount ++;
+}
+
+/*
+	Transmit hitte sensor
+*/
+void transmitHS(){
+	int gem = Htemp / HtempCount; // Set gemiddelde naar htemp / htempcount (totaal getal gedeeld door opgetelde hoeveelheid)
+	transmit(gem);
+	if(gem > HTH && status == 0){
+		simulateOUT();
+		status = 1;
+	}
+	if(gem < HTL && status == 1){
+		simulateIN();
+		status = 0;
+	}
+}
+
+//----------LichtSensor----------//
+int lichtSensor(){
+	return(ADCsingleREAD(0));
+}
+
+// add current value to total light value and count++
+void countLS(){
+	Lstand += lichtSensor();
+	LstandCount ++;
+}
+
+/*
+	Transmit licht sensor
+*/
+void transmitLS(){
+	int gem = Lstand / LstandCount; // Set gemiddelde naar Lstand / LstandCount (totaal getal gedeeld door opgetelde hoeveelheid)
+	transmit(gem);
+	if(gem > LTH && status == 0){
+		simulateOUT();
+		status = 1;
+	}
+	if(gem < LTL && status == 1){
+		simulateIN();
+		status = 0;
+	}
+}
+
+//----------DistanceSensor----------//
+
+int distanceSensor(){
+	on(PD7);
+	_delay_ms(10);
+	int duration = ADCsingleREAD(2);
+	off(PD7);
+	
+	int distance= duration*0.034/2;
+	
+	return distance;
+}
+
+void transmitDS(){
+	transmit(distanceSensor());
+}
+
+
+//				End SensorLogic				//
+//------------------------------------------//
+
+
+
+void simulate(){
+	int hs = hitteSensor();
+	int ls = lichtSensor();
+	
+//----------------------------//
+//Uncomment for instant result//
+
+// 	Licht sensor logic
+// 	if(ls > LTH && status == 0){
+// 		simulateOUT();
+// 		status = 1;
+// 	}
+// 	if(ls < LTL && status == 1){
+// 		simulateIN();
+// 		status = 0;
+// 	}
+// 	
+// 	// Hitte sensor logic
+// 	if(hs > HTH && status == 0){
+// 		simulateOUT();
+// 		status = 1;
+// 	}
+// 	if(hs < HTL && status == 1){
+// 		simulateIN();
+// 		status = 0;
+// 	}
+//----------------------------//
+}
+void start_init(){
+	on(PB4);	
+}
 int main()
 {
-	// Hier moeten nog enkele statements komen ....!
+	//inits
+	start_init();
+	uart_init();
+	SCH_Init_T1(); // zet timer op 0
+	
+	// counts
+	SCH_Add_Task(countHS,0,100);
+	SCH_Add_Task(countLS,0,100);
+	
+	// transmits
+	SCH_Add_Task(transmitHS,0,1000); // -> transmit to terminal
+	SCH_Add_Task(transmitLS,0,1000); 
+	SCH_Add_Task(transmitDS,0,1000); 
+	
+	
+	// simulate
+	SCH_Add_Task(simulate,0,100); 
+	
+	//start de scheduler
+	SCH_Start(); 
+	
+	_delay_ms(1000);
+	
+	while(1) {	
+		SCH_Dispatch_Tasks(); // zet een infinite loop voor de taken.
+	}
 	return 0;
 }
