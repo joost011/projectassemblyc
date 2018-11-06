c1b74998e500c4357879d6d44603349bd94e0227
mickelson050 <mikewierenga@hotmail.nl>
2018-11-06 19:40:22 +0100

    distance sensor in the making

--- arduino_code/sketch_oct23a/sketch_oct23a.ino
+++ arduino_code/sketch_oct23a/sketch_oct23a.ino
@@ -6,42 +6,22 @@
 *
 */
 // defines pins numbers
-const int trigPin = 9;
-const int echoPin = 10;
-const int lightPin = A1;
+
 const int tempPin = A0;
 
 // defines variables
 long duration;
-int distance;
-int lightValue; // variable to store the value coming from the sensor
+ // variable to store the value coming from the sensor
 int tempValue; // variable to store the value coming from the sensor
 double temp;
 
 void setup() {
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
 Serial.begin(9600); // Starts the serial communication
 }
 
 void loop() {
-// Clears the trigPin
-digitalWrite(trigPin, LOW);
-delay(1000);
 
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-
-// Calculating the distance
-distance= duration*0.034/2;
-
-// Read light value
-lightValue = analogRead(lightPin); // read the value from the sensor
 
 // Read temp value
 tempValue = analogRead(tempPin); // read the value from the sensor
@@ -51,6 +31,6 @@ temp = temp - 0.5;                   //Subtract the offset
 temp = temp * 100;                   //Convert to degrees 
 
 // Prints the distance on the Serial Monitor
-String StrDos = String(distance) + "," + String(lightValue) + "," + String(temp);
+String StrDos = String(temp);
 Serial.println(StrDos);
 }
